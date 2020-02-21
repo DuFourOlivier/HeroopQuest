3cd9e62d189acb66bec3f678e7a05f2864d03ff2
Olivier <Olivier.dufour@student.ap.be>
2020-02-21 11:54:44 +0100

    Ik heb helden

--- src_HeroopQuest/src_HeroopQuest/Program.cs
+++ src_HeroopQuest/src_HeroopQuest/Program.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using src_HeroopQuest.GameClasses;
+using System;
 
 namespace src_HeroopQuest
 {
     class Program
     {
-        static void Main(string[] args)        {
-         
+        static void Main(string[] args) 
+        {
+            Held held1 = new Held(HeldType.Barbaar,"Conan");
+            Held held2 = new Held(HeldType.Tovenaar, "Bart");
+
+            Console.WriteLine(held1.AanvalsDobbelstenen);
+            Console.WriteLine(held2.HeldType);
         }
+        
+
     }
 }
